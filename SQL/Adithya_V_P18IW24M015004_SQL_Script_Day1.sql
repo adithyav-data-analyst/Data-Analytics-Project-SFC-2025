09b7a6924a7392f90aaecd0f2562893048155a87
adithyav-data-analyst <adithya.20bcom@cambridge.edu.in>
2025-12-19 06:22:22 +0530

    Add files via upload
    This folder contains all the projects done in SQL taught by Petluri Edu Tech Dec, 2025

---
+++ SQL/Adithya_V_P18IW24M015004_SQL_Script_Day1.sql
@@ -0,0 +1,10 @@
+create database colleges;
+create database marks;
+ use colleges;
+ create table students_details(rollno int primary key, name varchar(20),age int);
+ select * from students_details;
+ insert into students_details values(101,"Adithya",23);
+ insert into students_details values(102,"Brian",24);
+ insert into students_details values(103,"Jeol",23);
+ insert into students_details values(104,"Hema",23);
+ drop table students_details;
